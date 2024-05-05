59f06cd93eefd6abde1330d5efd6f77dbeeb696b
Bari Ademi <baroademi@gmail.com>
2024-05-05 23:01:25 +0400

    test 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL:'http://asbajram.vercel.app',
+  COOKIE_VALUE:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJEQzIxZUROa0pubVpTTTRJSlcwcTRaYnYiLCJpYXQiOjE3MTQ5MzAzNjAsImF1ZCI6ImFzYmFqcmFtLTlhbDJ0eW5qcS1iYXJpMTIzcy1wcm9qZWN0cy52ZXJjZWwuYXBwIiwidXNlcm5hbWUiOiJiYXJpMTIzIiwic3ViIjoic3NvLXByb3RlY3Rpb24ifQ.g-fVd5gOKJ38gWpVuQidIQUwxqMJlBaaqN_9H7YqqPM'
 };
