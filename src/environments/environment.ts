3aa708dff075d13792c68855af33076d9d05ba9b
Bari Ademi <baroademi@gmail.com>
2024-05-06 03:12:10 +0400

    responsive fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL:'https://asbajram.vercel.app',
+  BASE_URL:'http://localhost:3000',
   COOKIE_VALUE:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJEQzIxZUROa0pubVpTTTRJSlcwcTRaYnYiLCJpYXQiOjE3MTQ5MzAzNjAsImF1ZCI6ImFzYmFqcmFtLTlhbDJ0eW5qcS1iYXJpMTIzcy1wcm9qZWN0cy52ZXJjZWwuYXBwIiwidXNlcm5hbWUiOiJiYXJpMTIzIiwic3ViIjoic3NvLXByb3RlY3Rpb24ifQ.g-fVd5gOKJ38gWpVuQidIQUwxqMJlBaaqN_9H7YqqPM'
 };
 
