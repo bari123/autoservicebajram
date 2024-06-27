f3321d03d60a43968a9ac9590c63f4adb8d99c72
Bari Ademi <baroademi@gmail.com>
2024-06-28 00:56:35 +0800

    final touches

--- src/components/dragdropcomp/dragdropcomp.component.ts
+++ src/components/dragdropcomp/dragdropcomp.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {CdkDragDrop} from "@angular/cdk/drag-drop";
 import {generateTimeslots} from "../../utils/timeslotGenerator.util";
 import {GlobalService} from "../../app/global.service";
@@ -9,13 +9,22 @@ import * as moment from "moment/moment";
   templateUrl: './dragdropcomp.component.html',
   styleUrls: ['./dragdropcomp.component.css']
 })
-export class DragdropcompComponent implements OnInit {
+export class DragdropcompComponent implements OnInit, OnChanges {
 
   @Output() newTimeSlot = new EventEmitter<any>();
 
   @Output() clicked = new EventEmitter<any>()
-  lifts: any;
 
+  @Input('date') date?: any
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['date']) {
+      this.loadAgenda()
+    }
+  }
+
+
+  lifts: any;
   agenda: any
   currentDate: moment.Moment = moment()
 
@@ -31,16 +40,18 @@ export class DragdropcompComponent implements OnInit {
     }));
   }
 
-  getGroup(value: any) {
-    let result:any=[]
-    this.result.filter((res: { value: any; indexes: any; }) => {
-        if (
-          res.value === value) {
-          result.push(res.indexes)
-        }
-      }
-    )
-    return result
+  async loadAgenda() {
+    let dateToSearch
+    if (this.date.toDate().toLocaleDateString() === this.currentDate.toDate().toLocaleDateString()) {
+      dateToSearch = this.currentDate.toDate().toLocaleDateString()
+    } else {
+      dateToSearch = this.date.toDate().toLocaleDateString()
+    }
+    this.agenda = await this.service.getAgenda(dateToSearch);
+    this.lifts = Array.from({length: 4}, (_, index) => ({
+      lift: index + 1,
+      timeslots: this.generateTimeslotsWithAgenda(index + 1)
+    }));
   }
 
 
@@ -77,31 +88,6 @@ export class DragdropcompComponent implements OnInit {
       oldLift
     });
   }
-
-
-  // drop(event: CdkDragDrop<any[]>, lift: any): void {
-  //   let oldLift
-  //   if (event.previousContainer === event.container) {
-  //     let value = this.lifts[lift.lift - 1].timeslots[event.currentIndex].value
-  //     let reserved = this.lifts[lift.lift - 1].timeslots[event.currentIndex].reserved
-  //     this.lifts[lift.lift - 1].timeslots[event.currentIndex].value = this.lifts[lift.lift - 1].timeslots[event.previousIndex].value
-  //     this.lifts[lift.lift - 1].timeslots[event.currentIndex].reserved = this.lifts[lift.lift - 1].timeslots[event.previousIndex].reserved
-  //     this.lifts[lift.lift - 1].timeslots[event.previousIndex].value = value
-  //     this.lifts[lift.lift - 1].timeslots[event.previousIndex].reserved = reserved
-  //   } else {
-  //     let liftIndex = event.container.id.slice(-1)
-  //     let liftIndexPrevious = event.previousContainer.id.slice(-1)
-  //     let value = this.lifts[liftIndex].timeslots[event.currentIndex].value
-  //     let reserved = this.lifts[liftIndex].timeslots[event.currentIndex].reserved
-  //     this.lifts[liftIndex].timeslots[event.currentIndex].value = this.lifts[liftIndexPrevious].timeslots[event.previousIndex].value
-  //     this.lifts[liftIndex].timeslots[event.currentIndex].reserved = this.lifts[liftIndexPrevious].timeslots[event.previousIndex].reserved
-  //     this.lifts[liftIndexPrevious].timeslots[event.previousIndex].value = value
-  //     this.lifts[liftIndexPrevious].timeslots[event.previousIndex].reserved = reserved
-  //     oldLift = parseInt(liftIndexPrevious)
-  //     oldLift = oldLift + 1
-  //   }
-  //   this.newTimeSlot.emit({lift: lift.lift, timeslots: this.lifts[lift.lift - 1].timeslots, oldLift});
-  // }
 
 
   trackByItem(index: number, item: any): any {
@@ -164,6 +150,4 @@ export class DragdropcompComponent implements OnInit {
     return timeslots;
   }
 
-
-  protected readonly generateTimeslots = generateTimeslots;
 }
